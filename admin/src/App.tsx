ba8f4f2f8d859fd3ae11ae4f94d7bb4bf4b2d800
BD sDirect <dbsdirect.sdm@smartdatainc.net>
2025-01-28 18:20:53 +0530

    Comment out Signup route in App component

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 // import { createBrowserRouter, RouterProvider } from 
 import Login from './Components/Login';
-import Signup from './Components/Signup';
+// import Signup from './Components/Signup';
 import Dashboard from './Components/Dashboard';
 import Profile from './Components/Profile';
 
@@ -12,7 +12,7 @@ function App() {
 
   const router = createBrowserRouter([
     { path: '/login', element: <Login /> },
-    { path: '/signup', element: <Signup /> },
+    // { path: '/signup', element: <Signup /> },
     { path: '/', element: <Login /> },
     { path: '/dashboard', element: <Dashboard /> },
     { path: '/profile', element: <Profile /> }
